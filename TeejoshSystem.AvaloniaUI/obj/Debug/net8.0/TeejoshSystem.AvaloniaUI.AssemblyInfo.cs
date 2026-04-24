e16b9a0488026a1c90f08639a75658df6a517fa7
ESBottgerI <esbottgeri@unjbg.edu.pe>
2026-04-23 19:26:59 -0500

    feat #02 - Validar stock como invariante de dominio

--- TeejoshSystem.AvaloniaUI/obj/Debug/net8.0/TeejoshSystem.AvaloniaUI.AssemblyInfo.cs
+++ TeejoshSystem.AvaloniaUI/obj/Debug/net8.0/TeejoshSystem.AvaloniaUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeejoshSystem.AvaloniaUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd716a4cbf993d1390a72d3ca57286b87444b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df3da382a5dec0380cf926aece09fc9d4d5916f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeejoshSystem.AvaloniaUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeejoshSystem.AvaloniaUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

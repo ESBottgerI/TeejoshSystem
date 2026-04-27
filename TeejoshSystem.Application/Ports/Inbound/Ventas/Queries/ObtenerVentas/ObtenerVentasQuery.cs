251558a7329bce2b558ffea455e0c798971ecaa2
ESBottgerI <esbottgeri@unjbg.edu.pe>
2026-04-26 22:15:13 -0500

    feat #4.2 - Commands/RegistrarVenta & Queries/ObtenerVentas

---
+++ TeejoshSystem.Application/Ports/Inbound/Ventas/Queries/ObtenerVentas/ObtenerVentasQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TeejoshSystem.Application.Common.Dtos;
+
+namespace TeejoshSystem.Application.Ports.Inbound.Ventas.Queries.ObtenerVentas
+{
+    public class ObtenerVentasQuery : IRequest<IReadOnlyList<VentaDto>>
+    {
+        public DateTime? Desde { get; }
+        public DateTime? Hasta { get; }
+
+        public ObtenerVentasQuery(DateTime? desde = null, DateTime? hasta = null)
+        {
+            Desde = desde;
+            Hasta = hasta;
+        }
+    }
+}

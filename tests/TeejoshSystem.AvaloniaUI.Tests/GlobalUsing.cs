07c461ed59b3319717178703840667310dd9e03b
ESBottgerI <esbottgeri@unjbg.edu.pe>
2026-05-05 18:56:41 -0500

    Tests 5/05/2026 Completados + Documentación

--- tests/TeejoshSystem.AvaloniaUI.Tests/GlobalUsing.cs
+++ tests/TeejoshSystem.AvaloniaUI.Tests/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using Xunit;
 global using FluentAssertions;
 global using NSubstitute;
+global using MediatR;
+global using TeejoshSystem.AvaloniaUI.Adapters.Inbound.Services;
+global using TeejoshSystem.AvaloniaUI.Adapters.Inbound.ViewModels.Common;
